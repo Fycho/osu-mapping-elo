7a5a35b55035296ac370ff2ab6f00f395724cf19
Fycho <yfvail@163.com>
2018-04-14 21:45:32 +0800

    fix incorrect function name

--- server/getUserIds.js
+++ server/getUserIds.js
@@ -2,14 +2,14 @@ var User = require('../model').User
 var input = require('../data/user2.json')
 var axios = require('axios')
 
-const apiKey = ''
+const apiKey = 'a188ca07f71ad1e153f81b4431a1d50aeb03e2a5'
 const baseUrl = `https://osu.ppy.sh/api/get_user?k=${apiKey}&u=`
 
 const getUserId = (username, id) => {
   console.log(`fetch user data: ${username}, ${id} \n`)
   axios.get(baseUrl + username).then(({data}) => {
     if (data.length > 0) {
-      if (data[0].username.toLowercase() === username.toLowercase()) {
+      if (data[0].username.toLowerCase() === username.toLowerCase()) {
         if (id && data[0].user_id != id) {
           console.log(`id not match: ${data[0].user_id}, ${id} \n`)
         }
