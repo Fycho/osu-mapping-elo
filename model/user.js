3ac6013ced778418059cfa958f8f28e089f1cf43
Fycho <yfvail@163.com>
2018-04-15 20:50:53 +0800

    修改pm2部署文件

--- model/user.js
+++ model/user.js
@@ -3,7 +3,7 @@
 module.exports = function (sequelize, DataTypes) {
   return sequelize.define('User', {
     id: { type: DataTypes.INTEGER(11), primaryKey: true, autoIncrement: true },
-    user_id: { type: DataTypes.INTEGER(11), allowNull: false },
+    user_id: { type: DataTypes.INTEGER(11), allowNull: false, unique: true },
     name: { type: DataTypes.STRING(64), allowNull: true,  unique: true, comment: 'osu!昵称' },
     elo: { type: DataTypes.DECIMAL(6, 2), allowNull: false, defaultValue: 1200.00, comment: '当前elo_score' },
     ranking: { type: DataTypes.INTEGER(11) }
